0b7fdcff178e96017647d50be44b0ed87fc6bf94
Serg Owtschinnikow <owtschinnikow@rambler.ru>
2021-11-16 16:46:31 +0300

    add function - id user ver 3

--- magazin_tg.py
+++ magazin_tg.py
@@ -31,11 +31,11 @@ def send_welcome(message):
                     '/cart - вывести список покупок',
                     '/id - id пользователя']
     bot.send_message(message.chat.id, '\n '.join(commatd_list))
-    print(user_id, user_message)  # подглядывание
+    print(user_id, message.chat.first_name, user_message)  # подглядывание
 
 
 @bot.message_handler(commands=['id'])
-def send_welcome(message):
+def send_id(message):
     """
     Функция выдаёт ID пользователя
     :param message: сообщение от пользователя
@@ -45,7 +45,7 @@ def send_welcome(message):
     user_id = message.chat.id
     answer_message = 'Ваш ID: ' + str(user_id)
     bot.send_message(message.chat.id, answer_message)
-    print(user_id, user_message)  # подглядывание
+    print(user_id, message.chat.first_name, user_message)  # подглядывание
 
 
 @bot.message_handler(commands=['start'])
@@ -58,7 +58,7 @@ def send_welcome(message):
     user_message = message.text
     user_id = message.chat.id
     bot.send_message(message.chat.id, "Hello! Делайте заказы!")
-    print(user_id, user_message)  # подглядывание
+    print(user_id, message.chat.first_name, user_message)  # подглядывание
 
 
 cart = {}
@@ -79,7 +79,7 @@ def buy_item(message):
     cart[user_id].append(item)
     answer_message = ('Товар ' + item + ' добавлен в корзину для ')
     bot.send_message(user_id, answer_message)
-    print(user_id, user_message)  # подглядывание
+    print(user_id, message.chat.first_name, user_message)  # подглядывание
 
 
 @bot.message_handler(commands=['cart'])
@@ -92,9 +92,17 @@ def buy_item(message):
     user_message = message.text
     user_id = message.chat.id
     items = cart[user_id]
-    answer_message = ('Ваша корзина: ' + ', '.join(items) )
+    answer_message = (str(message.chat.first_name) + '. Ваша корзина: ' + ', '.join(items) )
     bot.send_message(user_id, answer_message)
-    print(user_id, user_message)  # подглядывание
+    print(user_id, message.chat.first_name, user_message)  # подглядывание
+
+@bot.message_handler(commands=[''])
+def admin_function(message):
+    user_id = message.chat.id
+    bot.send_message(user_id, message.chat.first_name)
+    bot.send_message(user_id, message)
+    print(message.chat.first_name, message.text)
+    print(message.from_user.username, message.text)
 
 
 bot.polling()  # Запуск бота
